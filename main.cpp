dd787b0484b9af078b8821064311b0dd354e2006
mbatinefe <mbefe34@gmail.com>
2024-07-24 19:59:17 +0300

    Add File Stream, Store Stacks/Mazes in Memory

--- main.cpp
+++ main.cpp
@@ -8,31 +8,31 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <fstream>
 
 #include "stack.h"
 
 using namespace std;
 
+
 // Lets save the maze
-void saveMaze(Cell<int>** maze, int M, int N, int maze_id){
+void saveMaze(Cell<int>** maze, int M, int N, int K){
     // Lets save the maze to a file
     // We will save the maze as a text file
-    
-    string line = "     ";
-    cout << "     ";
-    for(int i = 0; i < N; i++){
-        line += "___";;
-    }
-    cout << line << "_____" << endl;
+
+    // Create the file
+    string filename = "maze_" + to_string(K) + ".txt";
+    ofstream file(filename);
+
+    // Write the maze to the file
+    file << M <<  " " << N << endl;
     for(int i = 0; i < M; i++){
-        cout << M << "    |";
         for(int j = 0; j < N; j++){
-            cout << maze[i][j].x << "     " << maze[i][j].y << " " << maze[i][j].L << " " << maze[i][j].R << " " << maze[i][j].U << " " << maze[i][j].D << " " << maze[i][j].visited << endl;
+            file << "x=" <<maze[M-i-1][j].x << " y=" << maze[M-i-1][j].y << " l=" << maze[M-i-1][j].L << " r=" << maze[M-i-1][j].R << " u=" << maze[M-i-1][j].U << " d=" << maze[M-i-1][j].D << endl;
         }
     }
-
+    file.close();
 }
-
 
 // Check if the current cell has unvisited neighbors
 bool CheckUVneighbor(Cell<int>** maze, int M, int N, Cell<int> c){
@@ -170,13 +170,11 @@ Cell<int> SelectNeighbor(Cell<int>**& maze, int M, int N, Cell<int>& c){
 }
 
 // Lets create a function to create maze
-void CreateMaze(int K, int M, int N){
+void CreateMaze(ListOfStacks<Cell<int>>& stacks_all, Cell<int>***& maze, int K, int M, int N){
     // I will use 3D arrays for this
     // K number of mazes and M       rows and N columns
     // So that I will have access to all mazes with one array
     
-    // Create the maze
-    Cell<int>*** maze = new Cell<int>**[K];    // Allocate memory for maze
     // Fill the maze with all walls according to the x-y rules
     // I want bottom left corner to be x=0, y=0 and top right to be x=N-1, y=M-1
     
@@ -193,7 +191,7 @@ void CreateMaze(int K, int M, int N){
                 maze[k][i][j].x = j;
                 maze[k][i][j].y = M-i-1; // We want bottom left to be 0,0
                 // If cell type is int, how can we store true or false? 
-                // We       w can use 1 for true and 0 for false
+                // We can use 1 for true and 0 for false
                 maze[k][i][j].L = 1;
                 maze[k][i][j].R = 1;
                 maze[k][i][j].U = 1;
@@ -238,8 +236,6 @@ void CreateMaze(int K, int M, int N){
         
     }
 
-    // Lets create our stack list
-    ListOfStacks<Cell<int>> stacks_all = ListOfStacks<Cell<int>>(K);
 
     // Lets add mazes to the stack's first element in order to list
     for(int k = 0; k < K; k++){
@@ -264,8 +260,6 @@ void CreateMaze(int K, int M, int N){
                 // temp_rest_base = [k][m][n]
                 askedStack->pop();
 
-                // Lets print temp_rest_base
-                
                 // Returned updated next cell
                 temp_rest_next = SelectNeighbor(maze[k], M, N, temp_rest_base);
                 // cout << "Selected Neighbor Rest: " << temp_rest.x << " " << temp_rest.y << endl;
@@ -328,9 +322,6 @@ void CreateMaze(int K, int M, int N){
 
             saveMaze(maze[k], M, N, k);
 
-            // Lets construct the maze path/wall algorithm now
-
-    
         } else {
             if (DEBUG_STACK) {
                 cout << "THERE IS NO STACK AVAILABLE" << endl;
@@ -352,6 +343,33 @@ void CreateMaze(int K, int M, int N){
 }
 
 
+void CreatePath(ListOfStacks<Cell<int>>& stacks_all, Cell<int>***& maze, int K, int M, int N, int entry_X, int entry_Y, int exit_X, int exit_Y){
+    // Lets create a path for the given maze
+    // We will use the stack to store the path
+
+    Stack<Cell<int>>* askedStack = stacks_all.getStackAtIndex(K-1);
+    askedStack->makeEmpty();
+
+    // Lets find the entry and exit points
+    Cell<int> entry = maze[K-1][M-entry_Y-1][entry_X];
+    Cell<int> exit = maze[K-1][M-exit_Y-1][exit_X];
+
+    // Check if we have the right entry and exit points
+    if(entry.x == entry_X && entry.y == entry_Y && exit.x == exit_X && exit.y == exit_Y){
+        cout << "Entry and Exit points are correct." << endl;
+
+        // Lets start from the entry point
+        askedStack->push(entry);
+        // Lets create the path
+        
+
+
+    } else {
+        cout << "Entry and Exit points are not correct." << endl;
+        return;
+    }
+
+}
 
 int main() {
     // Get input for maze number
@@ -366,8 +384,14 @@ int main() {
         // Assume M and N > 1
     cin >> M >> N;
 
+    // Lets create our stack list
+    ListOfStacks<Cell<int>> stacks_all = ListOfStacks<Cell<int>>(K);
+
     // Create the maze
-    CreateMaze(K, M, N);
+    Cell<int>*** maze = new Cell<int>**[K];
+
+    // Create the maze
+    CreateMaze(stacks_all, maze, K, M, N);
     cout << "All mazes are generated." << endl << endl;
 
     // Now, we will ask for entry, exit points and maze ID
@@ -382,7 +406,9 @@ int main() {
     cin >> exit_X >> exit_Y;
 
     // Now, lets figure it out the path
+    CreatePath(stacks_all, maze, maze_id, M, N, entry_X, entry_Y, exit_X, exit_Y);
 
-
+    // Deallocate maze and stacks_all
+    // Also, check for temp ones.
 
 }    
